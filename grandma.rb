35fc2d6609629132976bfefce4403236f161b156
Tandi Samples <tandisamples@gmail.com>
2019-08-18 12:54:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
    return "HUH?! SPEAK UP, SONNY!"
   elseif phrase == "I LOVE YOU GRANDMA!"
-   "NO, NOT SINCE 1938!"
+   return
  else
    "HUH?! SPEAK UP, SONNY!"
  end
