93be28925d6f84012652f19e635e4dbfb5d59524
Tandi Samples <tandisamples@gmail.com>
2019-08-18 12:32:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,8 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-   return "HUH?! SPEAK UP, SONNY"
-    elseif phrase =="I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+   return "I LOVE YOU TOO PUMPKIN!"
+    
   else
     return "NO, NOT SINCE 1938!"
 end
